a085fdcd694731c15d1b8682b926cde36448c68e
Elena Rakhimova <elenara@kth.se>
2019-11-22 14:25:16 +0100

    Added basic course memo model

---
+++ server/models/courseMemoModel.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+  _id: String,
+  courseCode: {
+    type: String,
+    trim: true,
+    required: [true, 'Enter Course Code']
+  },
+  semester: {
+    type: String,
+    trim: true,
+    minlength: 0,
+    default: ''
+  },
+  koppsGoals: {
+    type: String,
+    trim: true,
+    minlength: 0,
+    default: ''
+  },
+  previousTextsList: {
+    type: Array,
+    default: []
+  }
+})
+
+const CourseMemo = mongoose.model('CourseMemo', schema)
+module.exports = {
+  CourseMemo,
+  schema
+}
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   sample: require('./sample'),
+  courseMemoModel: require('./courseMemoModel')
 }

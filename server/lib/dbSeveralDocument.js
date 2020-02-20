5589f69b4500bf29acf80ecb38aad58a0fbc3a7c
Elena Rakhimova <elenara@kth.se>
2020-02-20 13:46:56 +0100

    Added function to extract used rounds for memos

--- server/lib/dbSeveralDocument.js
+++ server/lib/dbSeveralDocument.js
@@ -17,7 +17,47 @@ function getAllMemosByStatus(courseCode, status) {
   return doc
 }
 
+async function getLatestUsedRounds(courseCode, semester) {
+  if (!courseCode) throw new Error('courseCode must be set')
+  try {
+    log.debug('Fetching all courseMemos for ' + courseCode)
+    // const doc = CourseMemo.aggregate([{ $match: { courseCode, status: { $or: ['published', 'draft'] } } }])
+    const dbResponse = await CourseMemo.aggregate([{ $match: { courseCode, semester, status: 'draft' } }])
+    // const dbResponse = await db.fetchAllRoundAnalysisByCourseCodeAndSemester(courseCode.toUpperCase(), semester)
+    const returnObject = {
+      usedRounds: [],
+      publishedMemos: [],
+      draftMemos: []
+    }
+
+    let tempObject = {}
+    for (let index = 0; index < dbResponse.length; index++) {
+      tempObject = {
+        // user: dbResponse[index].changedBy,
+        status: dbResponse[index].status,
+        memoId: dbResponse[index]._id,
+        memoEndPoint: dbResponse[index].memoEndPoint
+        // ugKeys: dbResponse[index].ugKeys
+      }
+      if (tempObject.status === 'published') {
+        returnObject.publishedMemos.push(tempObject)
+      } else if (tempObject.status === 'draft') {
+        returnObject.draftMemos.push(tempObject)
+      }
+      returnObject.usedRounds.push(dbResponse[index].ladoksRoundsIds)
+    }
+    log.debug('Successfully got used round ids for', {
+      courseCode,
+      result: returnObject
+    })
+    return returnObject
+  } catch (error) {
+    return error
+  }
+}
+
 module.exports = {
   getAllMemosByCourseCode,
-  getAllMemosByStatus
+  getAllMemosByStatus,
+  getLatestUsedRounds
 }

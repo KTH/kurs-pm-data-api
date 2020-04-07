d5c3df0f833d20c82fca2479c9107d0ecf41177a
Elena Rakhimova <elenara@kth.se>
2020-04-07 15:17:49 +0200

    Added limit to bodyParser

--- server/server.js
+++ server/server.js
@@ -66,8 +66,10 @@ server.set('case sensitive routing', true)
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json({ limit: '50mb' }))
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 }))
+// server.use(bodyParser.json())
+// server.use(bodyParser.urlencoded({ extended: true }))
 server.use(cookieParser())
 
 /* ******************************

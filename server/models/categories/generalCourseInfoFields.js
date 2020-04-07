8bc50213a52427b1813d2e07abba1d72e35e879a
Elena Rakhimova <elenara@kth.se>
2020-04-07 11:18:52 +0200

    removed infoContactName

--- server/models/categories/generalCourseInfoFields.js
+++ server/models/categories/generalCourseInfoFields.js
@@ -7,12 +7,6 @@ const generalCourseInfo = {
     minlength: 0,
     default: ''
   },
-  infoContactName: {
-    type: String,
-    trim: true,
-    minlength: 0,
-    default: ''
-  },
   literature: {
     type: String,
     trim: true,

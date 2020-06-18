5044a463271eddd8b6b75ef78525350052c3f78f
Elena Rakhimova <elenara@kth.se>
2020-06-18 15:09:18 +0200

    Use async await to prevent some timeouts

--- server/lib/dbDataById.js
+++ server/lib/dbDataById.js
@@ -6,41 +6,44 @@ const { CourseMemo } = require('../models/mainMemoModel')
 /* ****** */
 /* ANY BY STATUS AND MemoEndPoint */
 /* ****** */
-function fetchMemoByEndPointAndStatus(memoEndPoint, status) {
+async function fetchMemoByEndPointAndStatus(memoEndPoint, status) {
   // UPDATED
   if (!memoEndPoint) throw new Error('memoEndPoint must be set')
   log.debug('Fetching memo based on ', { memoEndPoint, status })
-  return CourseMemo.findOne({ memoEndPoint, status }) // courseCode
+  const memo = await CourseMemo.findOne({ memoEndPoint, status }) // courseCode
+  return memo
 }
 
 /* ****** */
 /* DRAFT */
 /* ****** */
-function storeNewCourseMemoData(data) {
+async function storeNewCourseMemoData(data) {
   // ***** USED TO POST NEW COURSE MEMO FIRST DRAFT
   if (!data) throw new Error('Trying to post empty/innacurate data in storeNewCourseMemoData')
   else {
     data.lastChangeDate = new Date()
     const doc = new CourseMemo(data)
     log.debug('Create and store new FIRST draft in form of roundCourseMemoData', { doc })
-
-    return doc.save()
+    const result = await doc.save()
+    return result
   }
 }
 
-function updateMemoByEndPointAndStatus(data, status) {
+async function updateMemoByEndPointAndStatus(data, status) {
   // UPPDATERA DRAFT GENOM memoEndPoint
   const { memoEndPoint, courseCode } = data
   if (data) {
     log.debug('Update of existing memo or draft: ', { data })
-    return CourseMemo.findOneAndUpdate({ memoEndPoint, status, courseCode }, { $set: data })
+    const resultAfterUpdate = CourseMemo.findOneAndUpdate({ memoEndPoint, status, courseCode }, { $set: data })
+    return resultAfterUpdate
   }
   log.debug('No roundCourseMemoData found for updating it with new data', { data })
 }
 
-function removeCourseMemoDataById(id, courseCode) {
+async function removeCourseMemoDataById(id, courseCode) {
   log.debug('deleted roundCourseMemoData by ID: ', { id })
-  return CourseMemo.deleteOne({ _id: id, courseCode })
+  const deleted = await CourseMemo.deleteOne({ _id: id, courseCode })
+  return deleted
 }
 
 module.exports = {

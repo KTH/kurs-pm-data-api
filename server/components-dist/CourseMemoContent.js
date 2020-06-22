35b1c8ee7e1215ae3f49c7ed4c058bb69d07d7aa
Rickard Falk <rickard.falk@gmail.com>
2020-06-22 08:33:30 +0200

    First attempts at parsing HTML to React

--- server/components-dist/CourseMemoContent.js
+++ server/components-dist/CourseMemoContent.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -9,7 +11,11 @@ var _react = _interopRequireDefault(require("react"));
 
 var _renderer = require("@react-pdf/renderer");
 
-var _pdfUtils = require("../lib/pdfUtils");
+var _htmlReactParser = _interopRequireWildcard(require("html-react-parser"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -22,17 +28,44 @@ var styles = _renderer.StyleSheet.create({
   h2: {
     fontFamily: 'Open Sans SemiBold',
     fontSize: '18pt',
+    marginTop: '18pt',
     marginBottom: '9pt'
   }
 });
 
+var options = {
+  replace: function replace(domNode) {
+    if (domNode.type === 'tag' && domNode.name === 'ul') {
+      return /*#__PURE__*/_react["default"].createElement(_renderer.View, null, (0, _htmlReactParser.domToReact)(domNode.children, options));
+    }
+
+    if (domNode.type === 'tag' && domNode.name === 'li') {
+      return /*#__PURE__*/_react["default"].createElement(_renderer.View, null, (0, _htmlReactParser.domToReact)(domNode.children, options));
+    }
+
+    if (domNode.type === 'tag' && domNode.name === 'p') {
+      return /*#__PURE__*/_react["default"].createElement(_renderer.View, null, (0, _htmlReactParser.domToReact)(domNode.children, options));
+    }
+
+    if (domNode.type === 'text') {
+      return /*#__PURE__*/_react["default"].createElement(_renderer.Text, null, domNode.data);
+    }
+
+    return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null);
+  }
+};
+
 var CourseMemoContent = function CourseMemoContent(_ref) {
   var data = _ref.data;
+  var courseContent = (0, _htmlReactParser["default"])(data.courseContent);
+  var learningOutcomes = (0, _htmlReactParser["default"])(data.learningOutcomes, options);
   return /*#__PURE__*/_react["default"].createElement(_renderer.View, {
     style: styles.contentContainer
   }, /*#__PURE__*/_react["default"].createElement(_renderer.Text, {
     style: styles.h2
-  }, "Course Content"), /*#__PURE__*/_react["default"].createElement(_renderer.Text, null, (0, _pdfUtils.decodeHtml)(data.courseContent)));
+  }, "Course Content"), /*#__PURE__*/_react["default"].createElement(_renderer.Text, null, courseContent), /*#__PURE__*/_react["default"].createElement(_renderer.Text, {
+    style: styles.h2
+  }, "Learning Outcomes"), /*#__PURE__*/_react["default"].createElement(_renderer.Text, null, learningOutcomes));
 };
 
 var _default = CourseMemoContent;

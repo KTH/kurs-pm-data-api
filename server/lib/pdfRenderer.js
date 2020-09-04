912c4c8a53d872890348e5b14743a831831a3bbf
Rickard Falk <rickard.falk@gmail.com>
2020-09-04 07:42:45 +0200

    Fix for memory leak in PDF generation

--- server/lib/pdfRenderer.js
+++ server/lib/pdfRenderer.js
@@ -11,6 +11,13 @@ async function createPdf(writeStream, data) {
   // eslint-disable-next-line no-console
   console.timeEnd('createPdf: ReactPDF.renderToStream')
   doc.pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
+      resolve(doc)
+    })
+    writeStream.on('error', reject)
+  })
 }
 
 module.exports = {

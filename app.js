4983a8c999e061afa1c381ec2422de6da0a5dd16
Karl Andin <karl.andin@drakryggen.com>
2024-05-10 12:52:21 +0200

    feat(KUI-1306): switch to @kth/appinsights for application insights setup

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 'use strict'
-// Load .env file in development mode
-const nodeEnv = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase()
-if (nodeEnv === 'development' || nodeEnv === 'dev' || !nodeEnv) {
-  require('dotenv').config()
-} else if (!process.env.SERVICE_PUBLISH) {
-  // This is an ANSIBLE machine which doesn't set env-vars atm
-  // so read localSettings.js which we now use to fake env-vars
-  // because it already exists in our Ansible setup.
-  require('./config/localSettings')
-}
+
+require('dotenv').config()
+
+const { KthAppinsights } = require('@kth/appinsights')
+
+KthAppinsights.init({ name: 'kurs-pm-api' })
 
 const config = require('./server/configuration').server
 const server = require('./server/server')
